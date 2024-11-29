0fcd0e2fc2c67a2b1ae34be8434ffcdb4d89cec5
FCheatDev <fcheat.dev@gmail.com>
2024-11-29 16:09:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app } = require('electron');
 const path = require('path');
 
 // 導入配置和服務
-const CONFIG = require('./config/config');
+const CONFIG = require('./config/main.js');
 const logger = require('./assets/service/logger');
 const windowManager = require('./assets/service/WindowManager');
 const updateManager = require('./assets/service/UpdateManager');
